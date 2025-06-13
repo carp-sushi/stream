eae444141dfa6ba9623384beca64f0bb51677285
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-06-12 22:22:17 -0600

    add stream code and fib example

--- code/fib/app/Main.hs
+++ code/fib/app/Main.hs
@@ -1,4 +1,26 @@
 module Main (main) where
 
+import Control.Monad.Par
+import Par.Stream
+
+-- Calculate the n-th Fibonacci number using 0 to handle negatives.
+fib :: Integer -> Integer
+fib n
+    | n <= 0 = 0
+    | n == 1 = 1
+    | otherwise = fib (n - 1) + fib (n - 2)
+
+-- Use streams to calculate the sum of fibonacci numbers
+pipeline :: Int -> [Integer] -> Integer
+pipeline n xs =
+    runPar $ do
+        s0 <- streamFromList n xs -- emit list in batches of size `n`
+        s1 <- streamFilter even (n `div` 2) s0 -- emit even numbers + halve batch size
+        s2 <- streamMap fib n s1 -- calc fib number
+        sm <- streamFold (+) 0 s2 -- streaming sum
+        return sm
+
+-- Calculate and print the sum of some even Fibonacci numbers.
 main :: IO ()
-main = putStrLn "TODO"
+main = do
+    putStrLn $ show (pipeline 10 [1 .. 38])
--- code/stream/src/Par/Stream.hs
+++ code/stream/src/Par/Stream.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Par.Stream where
 
 import Control.DeepSeq
@@ -17,3 +19,77 @@ instance (NFData a) => NFData (IList a) where
     rnf Nil = ()
     rnf (Cons a b) = rnf a `seq` rnf b
     rnf (Fork op a) = seq op $ rnf a
+
+-- Convert a list into a batched stream.
+streamFromList :: (NFData a) => Int -> [a] -> Par (Stream a)
+streamFromList n xs = do
+    os <- new
+    fork $ loop n xs os
+    return os
+  where
+    loop _ [] o = put o Nil
+    loop 0 (h : t) o = do
+        v <- new
+        let op = loop n t v
+        put o (Fork op (Cons h v))
+    loop i (h : t) o = do
+        v <- new
+        put o (Cons h v)
+        loop (i - 1) t v
+
+-- Apply a fold operation to a stream yielding a single result.
+streamFold :: (a -> b -> a) -> a -> Stream b -> Par a
+streamFold fn !acc is = do
+    il <- get is
+    case il of
+        Nil -> return acc
+        Cons h t -> streamFold fn (fn acc h) t
+        Fork op (Cons h t) -> fork op >> streamFold fn (fn acc h) t
+
+-- Apply a map function to a stream producing another stream.
+streamMap :: (NFData b) => (a -> b) -> Int -> Stream a -> Par (Stream b)
+streamMap fn n is = do
+    os <- new
+    fork $ loop n is os
+    return os
+  where
+    loop x i o = do
+        il <- get i
+        case il of
+            Nil -> put o Nil
+            Cons h t -> proc x h t o
+            Fork op (Cons h t) -> fork op >> proc x h t o
+    proc x h t o
+        | x > 0 = do
+            v <- new
+            put o (Cons (fn h) v)
+            loop (x - 1) t v
+        | otherwise = do
+            v <- new
+            let op = loop n t v
+            put o (Fork op (Cons (fn h) v))
+
+-- Filter a stream using a predicate function producing another stream.
+streamFilter :: (NFData a) => (a -> Bool) -> Int -> Stream a -> Par (Stream a)
+streamFilter p n is = do
+    os <- new
+    fork $ loop n is os
+    return os
+  where
+    loop x i o = do
+        il <- get i
+        case il of
+            Nil -> put o Nil
+            Cons h t -> proc x h t o
+            Fork op (Cons h t) -> fork op >> proc x h t o
+    proc x h t o
+        | x > 0 && p h = do
+            v <- new
+            put_ o (Cons h v)
+            loop (x - 1) t v
+        | x <= 0 && p h = do
+            v <- new
+            let op = loop n t v
+            put_ o (Fork op (Cons h v))
+        | otherwise =
+            loop (x - 1) t o
